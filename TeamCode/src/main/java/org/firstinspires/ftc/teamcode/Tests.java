eb9f218a0ee8a6220d1900bc81051bb6d0aff4a3
taigertails-svg <taigertails@gmail.com>
2026-01-12 15:57:19 +0200

    Create Tests.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class Tests extends OpMode {
+    Shooter Shooter = new Shooter();
+    Drive Drive = new Drive();
+
+    @Override
+    public void init() {
+        Shooter.Init(hardwareMap);
+
+        Drive.Init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        Shooter.SetPower(0);
+        telemetry.addData("Shooter ", "OK");
+
+        // Test all drive methods
+        Drive.MoveMotors(0,0,0);
+        telemetry.addData("Drive: MoveMotors ","OK");
+        Drive.DriveFieldRelative(0,0,0);
+        telemetry.addData("Drive: DriveFieldRelative ","OK");
+        Drive.ResetIMU();
+        telemetry.addData("Drive: ResetIMU ","OK");
+
+        // Test joysticks
+        telemetry.addData("gamepad1.left_stick_x", gamepad1.left_stick_x);
+        telemetry.addData("gamepad1.left_stick_y", gamepad1.left_stick_y);
+        telemetry.addData("gamepad1.right_stick_x", gamepad1.right_stick_x);
+        telemetry.addData("gamepad1.right_stick_y", gamepad1.right_stick_y);
+
+        // Test buttons
+        telemetry.addData("gamepad1.a", gamepad1.a);
+        telemetry.addData("gamepad1.b", gamepad1.b);
+        telemetry.addData("gamepad1.x", gamepad1.x);
+        telemetry.addData("gamepad1.y", gamepad1.y);
+        telemetry.addData("gamepad1.ps", gamepad1.ps);
+    }
+}
