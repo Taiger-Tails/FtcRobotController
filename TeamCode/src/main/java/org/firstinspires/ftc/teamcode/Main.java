92832d616ba56a02c629b6eb43f39dbd3c97f94b
eitan25ch-create <taigertails@gmail.com>
2026-01-12 14:45:59 +0200

    Remove readme.md, add Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -5,9 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class Main extends OpMode {
+    // Make variables
     Drive Drive = new Drive();
     Shooter Shooter = new Shooter();
-
     double Forward, Strafe, Rotate;
 
     // Initialize driving
