5557dc9b300e7f2e1bcb3f5637afd8b720a2f90c
eitan25ch-create <taigertails@gmail.com>
2026-02-19 08:46:33 +0200

    Add PIDF to shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -1,15 +1,17 @@
-package org.firstinspires.ftc.teamcode;
+/package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class Shooter {
     // Make variables
     final private Constants Constants = new Constants();
 
-    public DcMotor ShooterMotor, ShooterMotor2;
+    public DcMotorEx ShooterMotor, ShooterMotor2;
     private CRServo ServoLeft, ServoRight;
 
     private double PreviousMotorRevs = 0;
@@ -18,12 +20,17 @@ public class Shooter {
 
     public void Init(HardwareMap HwMap) {
         // Set variables
-        ShooterMotor = HwMap.get(DcMotor.class, Constants.SHOOTER_WHEEL_NAME);
+        PIDFCoefficients PIDFCoefficients = new PIDFCoefficients(15, 1.0102, 12, 0);
+
+        ShooterMotor = HwMap.get(DcMotorEx.class, Constants.SHOOTER_WHEEL_NAME);
         ServoLeft = HwMap.get(CRServo.class, Constants.SERVO_LEFT_NAME);
         ServoRight = HwMap.get(CRServo.class, Constants.SERVO_RIGHT_NAME);
-        ShooterMotor2 = HwMap.get(DcMotor.class, Constants.SECOND_SHOOTER_WHEEL_NAME);
+        ShooterMotor2 = HwMap.get(DcMotorEx.class, Constants.SECOND_SHOOTER_WHEEL_NAME);
 
         ShooterMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        ShooterMotor2.setPIDFCoefficients(Constants.WHEEL_RUN_MODE, PIDFCoefficients);
+        ShooterMotor.setPIDFCoefficients(Constants.WHEEL_RUN_MODE, PIDFCoefficients);
 
         // Set it to break because when testing it wastes time by not coming to a complete stop
         ShooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
